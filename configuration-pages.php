450a996d1048170b1bbb2c99dcb6d868d3cb8bb7
Johan Janssens <johan@janssens.me>
2020-09-02 04:23:38 +0200

    Setup configuration

---
+++ configuration-pages.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+
+    'sites' => [
+        '[*]' => JPATH_ROOT.'/sites/agrea.ph',
+    ],
+
+    //See: https://www.owasp.org/index.php/OWASP_Secure_Headers_Project#xpcdp
+    'headers' => [
+        'Strict-Transport-Security' => 'max-age=63072000',
+        'X-Xss-Protection'          => '1; mode=block',
+        'X-Frame-Options'           => 'DENY',
+        'Feature-Policy'            => "camera 'none'; microphone 'none'",
+        'Referrer-Policy'           => 'strict-origin',
+        'X-Permitted-Cross-Domain-Policies' => 'none',
+        'Content-Security-Policy'   => 'upgrade-insecure-requests',
+        'X-Content-Type-Options'    => 'nosniff',
+    ],
+);
