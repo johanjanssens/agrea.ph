ef0ab839256dfabd9cde298945e2ed2d0b3bafc7
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-03 03:44:45 +0100

    Change the open and close icons for something more menu like

--- sites/agrea.ph/partials/navigation/submenu.html.php
+++ sites/agrea.ph/partials/navigation/submenu.html.php
@@ -6,11 +6,11 @@
 				<? if ($child->getChildren()): ?>
 					<li class="hover:bg-gray-100 items-center transition-colors duration-500 ease-in-out rounded-t p-2 flex justify-between relative<?= strpos(page()->path, $child->path) === 0 ? ' bg-gray-100 is-active' : '' ?>">
 						<a class="" href="<?= route($child) ?>"><?= $child->name ?></a>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 text-dark-green-500" :aria-expanded="isOpen" aria-controls="nav-list" :hidden="isOpen" @click="isOpen = !isOpen">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" />
+						<svg id="openMenu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-600" :aria-expanded="isOpen" aria-controls="nav-list" :hidden="isOpen" @click="isOpen = !isOpen">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
 						</svg>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 text-dark-green-500" :aria-expanded="isOpen" aria-controls="nav-list" :hidden="!isOpen" @click="isOpen = !isOpen">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
+						<svg id="closedMenu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-600" :aria-expanded="isOpen" aria-controls="nav-list" :hidden="!isOpen" @click="isOpen = !isOpen">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
 						</svg>
 					</li>
 					<ul :hidden="<?= isset ($menuOpen) ? '' : '!' ;?>isOpen" x-cloak id="nav-list" class="mb- pb-0">
