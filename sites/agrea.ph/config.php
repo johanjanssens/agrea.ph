0b26a0dd545a89594e757a699daa99cf0f2e0b17
Johan Janssens <johan@janssens.me>
2020-09-03 03:20:19 +0200

    Revert "ignore images and untrack configuration"
    This reverts commit a02195217efd6cb13ed92537c5aa9fdb54356c23.

---
+++ sites/agrea.ph/config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+
+    // Site
+    'site' => [
+        'body_class'        => '',
+        'copyright_notice'  => 'Agrea | All rights reserved',
+        'logo'              => 'theme://images/logo/website-logo.jpg',
+        'main_color'        => '#0089D6',
+        'name'              => 'AGREA',
+    ],
+
+    // Page
+    'page' => [
+
+        'metadata' => [
+            'og:site_name'  => 'Agrea - Ecology of Dignity',
+            'og:image'      => 'theme://images/logo/website-logo.jpg',
+            'twitter:site'  => '@AgreaPHL',
+            'twitter:card'  => 'summary_large_image',
+            //'fb:pages'     => '111111111111111'
+        ],
+
+        'visible'   => true,
+        'published' => true,
+    ],
+
+    'aliases' => [
+        'images://' => '/images/agrea.ph/pages/',
+    ],
+
+    // Google Analytics
+    'ga_code' => 'O-gAbCcD1E2FGHIJKlmnO3PqRst4Uv5wXzz-O1xx_xx'
+);
