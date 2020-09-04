c63f3faec3c0b7a67d2631f9f0c602b8837e3817
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-04 14:53:59 +0100

    Add weblinks yaml

--- sites/agrea.ph/pages/index.html.php
+++ sites/agrea.ph/pages/index.html.php
@@ -246,20 +246,22 @@ summary: The easy to use page generator for Joomla
 						<div class="h-1 w-20 bg-green-500 rounded"></div>
 					</div>
 				</div>
-				<div class="flex flex-wrap m-4">
+				<div class="grid sm:grid-cols-2 gap-2 m-4">
         <? foreach (data('weblinks') as $weblink) : ?>
-          <div class="rounded-lg h-64 overflow-hidden">
-            <a href="<?= $weblink->url; ?>"><img alt="<?= $weblink->title ?>" class="object-cover object-center h-full w-full" src="images://agrea-in-the-news/<?= $weblink->image_name ?>"></a>
+          <div class="bg-gray-200">
+            <div class="rounded-lg h-64 overflow-hidden">
+              <a href="<?= $weblink->url; ?>"><img alt="<?= $weblink->title ?>" class="object-cover object-center h-full w-full" src="images://agrea-in-the-news/<?= $weblink->image_name ?>" /></a>
+            </div>
+            <p class="leading-relaxed text-dark-green-500 text-xs font-medium"><?= $weblink->publication ?></p>
+            <p class="text-xs flex items-center mb-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg> <span><?= $weblink->date ?></span>
+            </p>
+            <h3 class="text-lg text-gray-900 font-medium font-title mb-4 leading-tight"><a href="<?= $weblink->url; ?>"><?= $weblink->title ?></a></h3>
           </div>
-          <p class="leading-relaxed text-dark-green-500 text-xs font-medium"><?= $weblink->publication ?></p>
-          <p class="text-xs flex items-center mb-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg> <span><?= $weblink->date ?></span>
-          </p>
-          <h3 class="text-lg text-gray-900 font-medium font-title mb-4 leading-tight"><a href="<?= $weblink->url; ?>"><?= $weblink->title ?></a></h3>
         <? endforeach; ?>
-				</div>
+        </div>
 			</div>
 		</div>
 	</div>
