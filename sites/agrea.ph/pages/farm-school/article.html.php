260d95605c29735ebc9c3eab8b1020a78e219c35
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-05 00:10:02 +0100

    sweep up the test code

--- sites/agrea.ph/pages/farm-school/article.html.php
+++ sites/agrea.ph/pages/farm-school/article.html.php
@@ -11,12 +11,3 @@ visible: false
 <?= import('/partials/articles/single.html', [
 	'article' => collection(),
 ]); ?>
-
-<div class="max-w-4xl m-auto mt-8">
-	<p>The following is a full list of all the team members alphabetically.</p>
-	<?= import('/partials/articles/list.html', [
-		'categoryID' => ['14',], // team
-		'categorySlug' => 'team',
-		'limit' => '6',
-	]); ?>
-</div>
--- sites/agrea.ph/pages/team/article.html.php
+++ sites/agrea.ph/pages/team/article.html.php
@@ -10,12 +10,3 @@ visible: false
 <?= import('/partials/articles/single.html', [
 	'article' => collection(),
 ]); ?>
-
-<div class="max-w-4xl m-auto mt-8">
-	<p>The following is a full list of all the jobs alphabetically.</p>
-	<?= import('/partials/articles/list.html', [
-		'categoryID' => ['13',], // farm-school
-		'categorySlug' => 'careers',
-		'limit' => '6',
-	]); ?>
-</div>
