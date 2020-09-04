95a2a96fd435e5e788f98b636450e8bf1ab1a579
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-04 18:14:56 +0100

    fix the position of the footer newsletter on mobile

--- sites/agrea.ph/partials/structure/footer.html.php
+++ sites/agrea.ph/partials/structure/footer.html.php
@@ -52,7 +52,7 @@
           </li>
         </ul>
       </div>
-      <div class="lg:w-1/4 md:w-1/2 w-full max-w-xs text-gray-900">
+      <div class="lg:w-1/4 md:w-1/2 sm:m-auto max-w-xs m-auto text-gray-900">
         <div class="flex content-between">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mx-2">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
