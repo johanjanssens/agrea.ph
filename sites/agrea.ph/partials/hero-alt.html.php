b34700c3191242e66ffe7edd369fcb969130b7d4
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-05 21:15:50 +0100

    update the hero image layout

--- sites/agrea.ph/partials/hero-alt.html.php
+++ sites/agrea.ph/partials/hero-alt.html.php
@@ -8,7 +8,7 @@
       'order' => 'desc',
       'limit' => 1,
     ]); ?>
-    <div class="md:flex md:justify-center md:w-1/2 py-24 sm:py-36 md:py-42 md:mx-0 mx-8">
+    <div class="md:flex md:justify-center md:w-2/3 py-24 sm:py-36 md:py-42 md:mx-0 mx-8">
         <div class="md:hidden mb-4">
           <img class="object-cover object-center rounded" src="<?= $articles->image->url ?>" alt="<?= $articles->title; ?>">
         </div>
@@ -23,8 +23,8 @@
         </div>
     </div>
   </div>
-  <div class="hero-image hidden md:block absolute top-0 right-0 z-10 h-full w-1/2">
-      <div style="background-image: url(<?= $articles->image->url ?>);background-position:right;" class="h-full object-cover">
+  <div class="hero-image hidden md:block absolute top-0 right-0 z-10 h-full w-1/3">
+      <div style="background-image: url(<?= $articles->image->url ?>);background-position:right top;" class="h-full object-cover">
           <div class="h-full bg-gray-900 opacity-25"></div>
       </div>
   </div>
