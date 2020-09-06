c2ca5341a50cdf44260e6ce7bd56db0bf2e68ae1
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-06 19:00:19 +0100

    Add correct code for getEditor()->getName()

--- sites/agrea.ph/partials/articles/single.html.php
+++ sites/agrea.ph/partials/articles/single.html.php
@@ -6,7 +6,9 @@
 	<div class="h-1 w-20 bg-green-500 rounded mb-6"></div>
 	<img itemprop="image" class="rounded w-full mb-6" src="<?= $article->image->url ?>" alt="<?= $article->title; ?>">
 	<p class="mt-2 text-xs font-medium flex flex-row justify-between">
-		<span itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name"><?= $article->editor ?></span></span>
+		<span itemprop="author" itemscope itemtype="http://schema.org/Person">
+			<span itemprop="name"><?= $article->getEditor()->getName() ?></span>
+		</span>
 		<span class="leading-relaxed text-dark-green-500 flex items-center">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
 			  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
