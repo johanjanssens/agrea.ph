a27552298516708da174a2ae4821898dcf9b6cd2
Johan Janssens <johan@janssens.me>
2020-09-09 06:28:22 +0200

    Set image filter priority to LOWEST

--- sites/agrea.ph/extensions/pages/template/filter/image.php
+++ sites/agrea.ph/extensions/pages/template/filter/image.php
@@ -2,6 +2,15 @@
 
 class ExtPagesTemplateFilterImage extends ComPagesTemplateFilterAbstract
 {
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'priority' => self::PRIORITY_LOWEST,
+        ));
+
+        parent::_initialize($config);
+    }
+
     public function filter(&$text)
     {
         $text = $this->getTemplate()->helper('image.filter', $text);
