3dc339c801337a6bf330c5ee571d34ab0ac86fc8
Johan Janssens <johan@janssens.me>
2020-09-09 20:37:13 +0200

    Enable responsive images

--- sites/agrea.ph/extensions/pages/config.php
+++ sites/agrea.ph/extensions/pages/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'ext:pages.template.helper.image' => [
-        'enable'    => getenv('SITE') ? false : false,
+        'enable'    => getenv('SITE') ? true : false,
         'suffix'    => 'php',
         'exclude'   => ['gif'],
         'max_width' => 1600,
