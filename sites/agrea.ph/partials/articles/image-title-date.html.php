f4f24dcdcae5e6e8a0188cdeea5830e8188542f9
Johan Janssens <johan@janssens.me>
2020-09-09 08:24:59 +0200

    Code cleanup

--- sites/agrea.ph/partials/articles/image-title-date.html.php
+++ sites/agrea.ph/partials/articles/image-title-date.html.php
@@ -1,7 +1,13 @@
 <div class="rounded-lg h-64 overflow-hidden">
-	<a href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>"><img alt="<?= article($id)->title ?>" class="object-cover object-center h-full w-full" src="<?= article($id)->image->url ?>"></a>
+	<a href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>">
+		<img alt="<?= article($id)->title ?>" class="object-cover object-center h-full w-full" src="<?= article($id)->image->url ?>">
+	</a>
 </div>
-<h3 class="font-medium text-gray-900 mt-6 mb-3"><a href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>"><?= article($id)->title ?></a></h3>
+<h3 class="font-medium text-gray-900 mt-6 mb-3">
+	<a href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>">
+		<?= article($id)->title ?>
+	</a>
+</h3>
 <p class="leading-relaxed text-xs flex items-center">
 	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
 	  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
--- sites/agrea.ph/partials/articles/image-title-excerpt.html.php
+++ sites/agrea.ph/partials/articles/image-title-excerpt.html.php
@@ -1,5 +1,11 @@
 <div class="rounded-lg h-32 overflow-hidden my-4">
-	<a href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>"><img alt="<?= article($id)->title ?>" class="h-40 rounded w-full object-cover object-center mb-6" src="<?= article($id)->image->url ?>"></a>
+	<a href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>">
+		<img alt="<?= article($id)->title ?>" class="h-40 rounded w-full object-cover object-center mb-6" src="<?= article($id)->image->url ?>">
+	</a>
 </div>
-<h3 class="font-medium text-gray-900 mb-4"><a class="text-gray-900 hover:text-gray-600" href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>"><?= article($id)->title ?></a></h3>
+<h3 class="font-medium text-gray-900 mb-4">
+	<a class="text-gray-900 hover:text-gray-600" href="<?= route(page($category . '/article'), ['slug' => article($id)->slug]) ?>">
+		<?= article($id)->title ?>
+	</a>
+</h3>
 <div class="mb-4"><?= article($id)->excerpt ?></div>
