501110c03d1a06461435e7e6b6549b0c543104db
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-09 17:07:02 +0100

    Reduce whitespace around newsletter signup form. remove signup checkbox, adjust the background svg so the farmer is more to the left to balace the footer out a tad

--- sites/agrea.ph/partials/subscription-form.html.php
+++ sites/agrea.ph/partials/subscription-form.html.php
@@ -1,4 +1,4 @@
-<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ml-auto mr-auto">
+<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 ml-auto mr-auto">
 	<div class="mb-4">
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="fullname">
 			Full Name
@@ -10,14 +10,6 @@
 			Email Address
 		</label>
 		<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="hello@example.ph" value="">
-	</div>
-	<div class="mb-6">
-		<label class="block text-gray-500 font-bold">
-			<input  class="switch" type="checkbox">
-			<span class="text-sm">
-				Send me your newsletter!
-			</span>
-		</label>
 	</div>
 	<div class="flex items-center justify-between">
 		<button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
