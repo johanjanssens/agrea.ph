5eb439f56615c239b755c852e49431d55b1b38dd
Johan Janssens <johan@janssens.me>
2020-09-14 01:59:08 +0200

    Add support for responsive background images

--- sites/agrea.ph/extensions/pages/config.php
+++ sites/agrea.ph/extensions/pages/config.php
@@ -1,10 +1,12 @@
 <?php
 return [
     'ext:pages.template.helper.image' => [
-        'enable'    => getenv('SITE') ? true : false,
         'suffix'    => getenv('SITE') ? 'php': '',
         'exclude'   => ['gif'],
         'max_width' => 1280,
         'base_path' => JPATH_ROOT,
     ],
+    'ext:pages.template.filter.image'   => [
+         'enable'    => getenv('SITE') ? true : false,
+   ],
 ];
--- sites/agrea.ph/extensions/pages/template/filter/image.php
+++ sites/agrea.ph/extensions/pages/template/filter/image.php
@@ -6,9 +6,15 @@ class ExtPagesTemplateFilterImage extends ComPagesTemplateFilterAbstract
     {
         $config->append(array(
             'priority' => self::PRIORITY_LOWEST,
+            'enable'   => JDEBUG ? false : true,
         ));
 
         parent::_initialize($config);
+    }
+
+    public function enabled()
+    {
+        return (bool) $this->getConfig()->enable;
     }
 
     public function filter(&$text)
@@ -17,7 +23,7 @@ class ExtPagesTemplateFilterImage extends ComPagesTemplateFilterAbstract
         if($this->getTemplate()->getLayout() !== NULL)
         {
             $matches = array();
-            //First pass
+            //First pass - Find images between <ktml:images></ktml:images>
             if(preg_match_all('#<ktml:images(.*)>(.*)<\/ktml:images>#siU', $text, $matches))
             {
                 foreach($matches[0] as $key => $match)
@@ -29,13 +35,154 @@ class ExtPagesTemplateFilterImage extends ComPagesTemplateFilterAbstract
                         $attribs['class'] = explode(' ', $attribs['class']);
                     }
 
-                    $result = $this->getTemplate()->helper('image.filter', $matches[2][$key], $attribs);
-                    $text   = str_replace($match, $result, $text);
+                    //Filter image elements
+                    $result = $this->filterImages($matches[2][$key], $attribs);
+
+                    //Filter background images
+                    $result = $this->filterBackgroundImages($result);
+
+                    $text = str_replace($match, $result, $text);
                 }
             }
 
-            //Second pass
-            $text = $this->getTemplate()->helper('image.filter', $text);
+            //Second pass - Filter image elements
+            $text =  $this->filterImages($text);
+
+            //Second pass- Filter the background images
+            $text = $this->filterBackgroundImages($text);
         }
+    }
+
+    public function filterImages($html, $config = array())
+    {
+        if($this->enabled())
+        {
+            $matches = array();
+            if(preg_match_all('#<img\s([^>]*?[\'\"][^>]*?)>(?!\s*<\/noscript>)#siU', $html, $matches))
+            {
+                foreach($matches[1] as $key => $match)
+                {
+                    $attribs = $this->parseAttributes($match);
+                    $src     = $attribs['src'] ?? null;
+                    $valid   = !isset($attribs['srcset']) && !isset($atrribs['data-srcset']) && !isset($attribs['data-src']);
+
+                    //Only handle none responsive supported images
+                    if($src && $valid && $this->getTemplate()->helper('image.supported', $src))
+                    {
+                        //Convert class to array
+                        if(isset($attribs['class'])) {
+                            $attribs['class'] = explode(' ', $attribs['class']);
+                        }
+
+                        $attribs['url'] = '/'.ltrim($src, '/');
+                        unset($attribs['src']);
+
+                        foreach($attribs as $name => $value)
+                        {
+                            if(strpos($name, 'data-') !== false)
+                            {
+                                unset($attribs[$name]);
+
+                                $name = str_replace('data-', '', $name);
+
+                                if($value === 'true') {
+                                    $value = true;
+                                }
+
+                                if($value === 'false') {
+                                    $value = false;
+                                }
+
+                                $attribs[$name] = $value;
+                            }
+                        }
+
+                        //Replace config and attribs
+                        $attribs = array_replace_recursive($config, $attribs);
+
+                        //Rename hyphen to underscore
+                        $options = array();
+                        foreach($attribs as $name => $value)
+                        {
+                            $name = str_replace('-', '_', $name);
+                            $options[$name] = $value;
+                        }
+
+                        //Filter the images
+                        $html = str_replace($matches[0][$key], $this->getTemplate()->helper('image', $options), $html);
+                    }
+                }
+            }
+        }
+
+        return $html;
+    }
+
+    public function filterBackgroundImages($html, $config = array())
+    {
+        if($this->enabled())
+        {
+            $matches = array();
+            if(preg_match_all('#<\S+\s+(.*(background-image\s*:\s*url\((.+)\);*?).+)>#iU', $html, $matches))
+            {
+                foreach($matches[1] as $key => $match)
+                {
+                    $html .= $this->getTemplate()->helper('image.import', 'bgset');
+
+                    $attribs = $this->parseAttributes($match);
+
+                    foreach($attribs as $name => $value)
+                    {
+                        if(strpos($name, 'data-') !== false)
+                        {
+                            unset($attribs[$name]);
+
+                            $name = str_replace('data-', '', $name);
+
+                            if($value === 'true') {
+                                $value = true;
+                            }
+
+                            if($value === 'false') {
+                                $value = false;
+                            }
+
+                            $attribs[$name] = $value;
+                        }
+                    }
+
+                    //Replace config and attribs
+                    $attribs = array_replace_recursive($config, $attribs);
+
+                    //Rename hyphen to underscore
+                    $options = array();
+                    foreach($attribs as $name => $value)
+                    {
+                        $name = str_replace('-', '_', $name);
+                        $options[$name] = $value;
+                    }
+
+                    if($srcset = $this->getTemplate()->helper('image.srcset', $matches[3][$key], $options))
+                    {
+                        $attribs['data-sizes'] = 'auto';
+                        $attribs['data-bgset'] = implode(',', $srcset);
+                        $attribs['class']      = $attribs['class'].' lazyload';
+
+                        $attribs['style'] = str_replace($matches[2][$key], '', $attribs['style']);
+
+                        if(empty(trim($attribs['style'], '"'))) {
+                            unset($attribs['style']);
+                        }
+
+                        $attribs = $this->buildAttributes($attribs);
+
+                        //Filter the images
+                        $html = str_replace($matches[1][$key], $attribs, $html);
+                    }
+                }
+            }
+        }
+
+        return $html;
     }
 }
