b01c97dc74d083c8aa30baa26f66a6b9a0ba6dc6
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-17 18:23:29 +0100

    Remove the webisite link in the team sidebar and add an RSS link

--- sites/agrea.ph/pages/team/index.html.php
+++ sites/agrea.ph/pages/team/index.html.php
@@ -101,12 +101,16 @@ summary: AGREA aims to help eradicate poverty for farming and fishing families, 
 	            <span class="ml-2 text-green-500 hover:text-green-700">@agreaphl</span>
 	          </a>
 	      	</li>
+	    </ul>
+		<h3 class="text-gray-900 font-medium font-title my-4">Syndicated News:</h3>
+		<div class="w-12 h-1 bg-green-500 rounded mt-2 mb-4"></div>
+		<ul class="mb-2">
 			<li>
-	          <a href="/" title="Follow us on twitter" class="inline-flex transition duration-500 ease-in-out">
-	            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5" stroke="currentColor">
-				  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+	          <a href="/news.rss" title="Follow us on twitter" class="inline-flex transition duration-500 ease-in-out">
+	            <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-5 h-5" viewBox="0 0 24 24" stroke="currentColor">
+				  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
 				</svg>
-	            <span class="ml-2 text-green-500 hover:text-green-700">website</span>
+	            <span class="ml-2 text-green-500 hover:text-green-700">RSS News Feed</span>
 	          </a>
 	      	</li>
 	    </ul>
