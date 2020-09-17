d0deea44f7462303669bdcadabb411781c8da43c
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-17 13:30:23 +0100

    float the profile image on medium and large screens

--- sites/agrea.ph/pages/team/rafael-seno.html.php
+++ sites/agrea.ph/pages/team/rafael-seno.html.php
@@ -11,8 +11,8 @@ slug: rafael-seno
 	<h1 itemprop="name" class="sm:text-5xl text-4xl font-medium font-title mb-2 text-gray-900 leading-none"><?= $title ?></h1>
 	<p itemprop="jobTitle" class="mb-2 text-sm font-medium"><?= $position ?></p>
 	<div class="h-1 w-20 bg-green-500 rounded mb-6"></div>
-	<img itemprop="image" class="rounded w-full mb-6" src="images://team/<?= $slug ?>.jpg" alt="<?= $title ?>">
-	<div class="mt-2" itemprop="articleBody">
+	<img itemprop="image" class="rounded md:w-1/4 md:float-right md:ml-4 mb-6" src="images://team/<?= $slug ?>.jpg" alt="<?= $title ?>">
+	<div class="md:inline" itemprop="articleBody">
 		<p>Rafa is a Filipino youth leader, an events host and organiser in Marinduque, a graphics and branding designer, environmental advocate, and Rapport and Communications Director at AGREA.</p>
 		<p>In 2000, Rafa took Electronics and Communications Engineering at the New Era University in Quezon City. From 2004 to 2012, he worked in various national and multi-national companies such as eTelecare Global Solutions, [24]7 Customer Philippines, JP Morgan Chase & Co., and Thomson Reuters. He returned to Marinduque in 2012 and took a Bachelor of Arts Degree in Communication at the Marinduque State College.</p>
 		<p>Some of his achievements include: Second Prize Winner (Group Category) and Leader of the Year Top 10 National Finalist (Solo Category) during the “El Liderato: First National Leadership Masters Challenge” in December 2014 in Camarines Sur; International Head of the Global Extemporaneous Speech Face-off and Overall Technical Director at the “Dux Gregis: First International Youth Leaders Convergence” in June 2015 in Ho Chi Minh City, Vietnam. He is also currently the Chapter President of AFS (American Field Service) Intercultural Programs Philippines–Marinduque.</p>
