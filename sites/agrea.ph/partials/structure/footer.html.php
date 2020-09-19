9be895d190b2525b73d9ad5b0cf26b18d91066da
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-19 18:56:19 +0100

    Give the social media icons a colour

--- sites/agrea.ph/partials/structure/footer.html.php
+++ sites/agrea.ph/partials/structure/footer.html.php
@@ -44,18 +44,18 @@
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
         <span class="text-gray-500 text-xs sm:ml-4 sm:mt-0 hidden md:inline">Follow our social feeds</span>
         <span class="inline-flex mx-2 -mt-2">
-          <a href="https://www.facebook.com/agreaph" title="Follow us on facebook" class="bg-green-500 rounded border-green-900 border p-1 text-dark-green-600 hover:bg-green-200 transition duration-500 ease-in-out">
+          <a href="https://www.facebook.com/agreaph" title="Follow us on facebook" class="bg-green-500 rounded border-green-900 border p-1 text-blue-800 hover:bg-green-200 transition duration-500 ease-in-out">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
-          <a href="https://www.instagram.com/agreaphl/" title="Follow our instagram" class="bg-green-500 rounded border-green-900 border p-1 ml-1 text-dark-green-600 hover:bg-green-200 transition duration-500 ease-in-out">
+          <a href="https://www.instagram.com/agreaphl/" title="Follow our instagram" class="bg-green-500 rounded border-green-900 border p-1 ml-1 text-gray-900 hover:bg-green-200 transition duration-500 ease-in-out">
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a href="https://www.twitter.com/agreaphl" title="Follow us on twitter" class="bg-green-500 rounded border-green-900 border p-1 ml-1 text-dark-green-600 hover:bg-green-200 transition duration-500 ease-in-out">
+          <a href="https://www.twitter.com/agreaphl" title="Follow us on twitter" class="bg-green-500 rounded border-green-900 border p-1 ml-1 text-blue-400 hover:bg-green-200 transition duration-500 ease-in-out">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
