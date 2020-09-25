17d564c27c3da5cd084d8cb6b40bb3ea85b0c335
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-26 00:29:52 +0100

    Add a basic thank you page

---
+++ sites/agrea.ph/pages/thank-you.html.php
@@ -0,0 +1,13 @@
+---
+layout: default
+name: Thank You
+slug: thank-you
+title: Thank you for contacting us
+summary: Thank you for contacting us
+metadata:
+    robots: [noindex, nofollow]
+---
+<div class="align-center">
+    <h1>Thank you for contacting us!</h1>
+    <p>We've received your enquiry and we will be in touch as soon as possible.</p>
+</div>
