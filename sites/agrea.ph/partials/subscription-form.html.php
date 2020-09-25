2c32c8075ab0f2fcbd377d1f218f8989518628f8
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-26 00:24:52 +0100

    Split up fullname into firstname / lastname

--- sites/agrea.ph/partials/subscription-form.html.php
+++ sites/agrea.ph/partials/subscription-form.html.php
@@ -1,9 +1,17 @@
 <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 ml-auto mr-auto">
-	<div class="mb-4">
-		<label class="block text-gray-700 text-sm font-bold mb-2" for="fullname">
-			Full Name
-		</label>
-		<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fullname" type="text" placeholder="Jane Doe" value="">
+	<div class="grid md:grid-cols-2 gap-4 mb-4">
+		<div class="mb-6 md:mb-0">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="grid-first-name">
+			First Name
+			</label>
+			<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="grid-first-name" type="text" placeholder="Jane">
+		</div>
+		<div class="mb-6 md:mb-0">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="grid-last-name">
+			Last Name
+			</label>
+			<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="grid-last-name" type="text" placeholder="Doe">
+		</div>
 	</div>
 	<div class="mb-6">
 		<label class="block text-gray-700 text-sm font-bold mb-2" for="email">
