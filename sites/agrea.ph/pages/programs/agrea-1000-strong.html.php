161e65a314d85285184a687b74c2e7a8a569519f
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-27 16:06:25 +0100

    Move the 1000 strong form into the page body and delete the job application form

--- sites/agrea.ph/pages/programs/agrea-1000-strong.html.php
+++ sites/agrea.ph/pages/programs/agrea-1000-strong.html.php
@@ -22,9 +22,119 @@ slug: agrea-1000-strong
 		</div>
 		<div itemprop="articleBody">
 		<ktml:images max-width="80%">
+		
 			<?= article(4)->excerpt ?>
 			<?= import('/partials/slider', ['class' => ['mb-8'], 'folder' => 'images://slider/1000strong', 'max_width' => '80%']); ?>
 			<?= article(4)->text ?>
+
+			<img alt="Be a 1000Strong Volunteer" class="rounded w-full my-6" src="images://programs/1000strong/1000strong-5.jpg" />
+			<h3 class="text-gray-900 font-medium font-title my-4 leading-tight">Be an AGREA 1000 Strong Volunteer</h3>
+			<div class="w-12 h-1 bg-green-500 rounded mt-2 mb-4"></div>
+			<p class="text-gray-900 my-4">Fill-out and submit the form below to receive updates and other opportunities available for AGREA 1000 Strong volunteers.</p>
+			<form class="text-gray-900 mb-8">
+				<fieldset class="mb-4">
+					<div class="question">
+						<label class="block">Name<sup>*</sup></label>
+						<div class="grid grid-cols-2">
+							<input class="sm:mr-2 bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4" placeholder="First Name" type="text" name="first-name">
+							<input class="bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4" placeholder="Last Name" type="text" name="last-name">
+						</div>
+					</div>
+					<div class="question">
+						<label for="email" class="block">Email Address<sup>*</sup></label>
+						<input class="w-auto sm:w-full bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4" placeholder="Email Address" type="text" name="email">
+					</div>
+				</fieldset>
+				<div class="grid sm:grid-cols-2">
+					<fieldset class="mb-4">
+						<h4 class="text-sm text-gray-900 font-medium font-title my-4 leading-tight">Choose as many volunteer sectors that apply to you<sup>*</sup></h4>
+						<div class="question">
+							<div>
+								<input class="switch" type="checkbox" id="male" name="male" value="Male Youth (13 to 35 years old)">
+								<label class="text-sm" for="male"> Male Youth (13 to 35 years old)</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="female" name="female" value="Female Youth (13 to 35 years old)">
+								<label class="text-sm" for="female"> Female Youth (13 to 35 years old)</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="mother" name="mother" value="Mother">
+								<label class="text-sm" for="mother"> Mother</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="farmer-leader" name="farmer-leader" value="Farmer Leader">
+								<label class="text-sm" for="farmer-leader"> Farmer Leader</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="fisherfolk-leader" name="fisherfolk-leader" value="Fisherfolk Leader">
+								<label class="text-sm" for="fisherfolk-leader"> Fisherfolk Leader</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="lgbt" name="lgbt" value="LGBT+">
+								<label class="text-sm" for="lgbt"> LGBT+</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="teacher" name="teacher" value="Teacher/Educator">
+								<label class="text-sm" for="teacher"> Teacher/Educator</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="disability" name="disability" value="Person with Disability">
+								<label class="text-sm" for="disability"> Person with Disability</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="senior" name="senior" value="Senior Citizen">
+								<label class="text-sm" for="senior"> Senior Citizen</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="overseas" name="overseas" value="Overseas Filipino Worker">
+								<label class="text-sm" for="overseas"> Overseas Filipino Worker</label><br>
+							</div>
+						</div>
+					</fieldset>
+					<fieldset class="mb-4">
+						<h4 class="text-sm text-gray-900 font-medium font-title my-4 leading-tight">Interests<sup>*</sup></h4>
+						<div class="question">
+							<div>
+								<input class="switch" type="checkbox" id="environmental" name="environmental" value="Environmental protection">
+								<label class="text-sm" for="environmental"> Environmental protection</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="farm-development" name="farm-development" value="Organic farm development">
+								<label class="text-sm" for="farm-development"> Organic farm development</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="volunteer" name="volunteer" value="AGREA Farm School volunteer">
+								<label class="text-sm" for="volunteer"> AGREA Farm School volunteer</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="garden-development" name="garden-development" value="Elementary school garden development">
+								<label class="text-sm" for="garden-development"> School garden development</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="extension" name="extension" value="Agricultural extension program">
+								<label class="text-sm" for="extension"> Agricultural extension program</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="leadership" name="leadership" value="Youth leadership events">
+								<label class="text-sm" for="leadership"> Youth leadership events</label><br>
+							</div>
+							<div>
+								<input class="switch" type="checkbox" id="program-development" name="program-development" value="Program development">
+								<label class="text-sm" for="program-development"> Program development</label><br>
+							</div>
+						</div>
+					</fieldset>
+				</div>
+				<fieldset class="">
+					<h4 class="text-sm text-gray-900 font-medium font-title my-4 leading-tight">Describe your volunteering and leadership experience (if any)</h4>
+					<div class="question">
+						<textarea name="experience" rows="5" cols="15" class="sm:w-full bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4"></textarea>
+					</div>
+				</fieldset>
+				<div class="submit">
+					<button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded" type="submit">Submit</button>
+				</div>
+			</form>
 		</ktml:images>
 		</div>
 	</article>
@@ -33,112 +143,6 @@ slug: agrea-1000-strong
 		<h3 class="font-medium font-title mt-4 text-gray-900 text-lg">In this section</h3>
 		<div class="w-12 h-1 bg-green-500 rounded mt-2 mb-4"></div>
 		<?= import('/partials/navigation/submenu');?>
-
-		<img alt="Be a 1000Strong Volunteer" class="rounded w-full my-6" src="images://programs/1000strong/1000strong-5.jpg" />
-		<h3 class="text-gray-900 font-medium font-title my-4 leading-tight">Be an AGREA 1000 Strong Volunteer</h3>
-		<div class="w-12 h-1 bg-green-500 rounded mt-2 mb-4"></div>
-		<p class="text-gray-900 my-4">Fill-out and submit the form below to receive updates and other opportunities available for AGREA 1000 Strong volunteers.</p>
-		<form class="text-gray-900 mb-8">
-			<fieldset class="mb-4">
-				<div class="question">
-					<label class="block">Name<sup>*</sup></label>
-					<div class="grid grid-cols-2">
-						<input class="sm:mr-2 bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4" placeholder="First Name" type="text" name="first-name">
-						<input class="bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4" placeholder="Last Name" type="text" name="last-name">
-					</div>
-				</div>
-				<div class="question">
-					<label for="email" class="block">Email Address<sup>*</sup></label>
-					<input class="w-auto sm:w-full bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4" placeholder="Email Address" type="text" name="email">
-				</div>
-			</fieldset>
-			<fieldset class="mb-4">
-				<h4 class="text-sm text-gray-900 font-medium font-title my-4 leading-tight">Choose as many volunteer sectors that apply to you<sup>*</sup></h4>
-				<div class="question">
-					<div>
-						<input class="switch" type="checkbox" id="male" name="male" value="Male Youth (13 to 35 years old)">
-						<label class="text-sm" for="male"> Male Youth (13 to 35 years old)</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="female" name="female" value="Female Youth (13 to 35 years old)">
-						<label class="text-sm" for="female"> Female Youth (13 to 35 years old)</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="mother" name="mother" value="Mother">
-						<label class="text-sm" for="mother"> Mother</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="farmer-leader" name="farmer-leader" value="Farmer Leader">
-						<label class="text-sm" for="farmer-leader"> Farmer Leader</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="fisherfolk-leader" name="fisherfolk-leader" value="Fisherfolk Leader">
-						<label class="text-sm" for="fisherfolk-leader"> Fisherfolk Leader</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="lgbt" name="lgbt" value="LGBT+">
-						<label class="text-sm" for="lgbt"> LGBT+</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="teacher" name="teacher" value="Teacher/Educator">
-						<label class="text-sm" for="teacher"> Teacher/Educator</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="disability" name="disability" value="Person with Disability">
-						<label class="text-sm" for="disability"> Person with Disability</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="senior" name="senior" value="Senior Citizen">
-						<label class="text-sm" for="senior"> Senior Citizen</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="overseas" name="overseas" value="Overseas Filipino Worker">
-						<label class="text-sm" for="overseas"> Overseas Filipino Worker</label><br>
-					</div>
-				</div>
-			</fieldset>
-			<fieldset class="mb-4">
-				<h4 class="text-sm text-gray-900 font-medium font-title my-4 leading-tight">Interests<sup>*</sup></h4>
-				<div class="question">
-					<div>
-						<input class="switch" type="checkbox" id="environmental" name="environmental" value="Environmental protection">
-						<label class="text-sm" for="environmental"> Environmental protection</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="farm-development" name="farm-development" value="Organic farm development">
-						<label class="text-sm" for="farm-development"> Organic farm development</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="volunteer" name="volunteer" value="AGREA Farm School volunteer">
-						<label class="text-sm" for="volunteer"> AGREA Farm School volunteer</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="garden-development" name="garden-development" value="Elementary school garden development">
-						<label class="text-sm" for="garden-development"> School garden development</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="extension" name="extension" value="Agricultural extension program">
-						<label class="text-sm" for="extension"> Agricultural extension program</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="leadership" name="leadership" value="Youth leadership events">
-						<label class="text-sm" for="leadership"> Youth leadership events</label><br>
-					</div>
-					<div>
-						<input class="switch" type="checkbox" id="program-development" name="program-development" value="Program development">
-						<label class="text-sm" for="program-development"> Program development</label><br>
-					</div>
-			</fieldset>
-			<fieldset class="">
-				<h4 class="text-sm text-gray-900 font-medium font-title my-4 leading-tight">Describe your volunteering and leadership experience (if any)</h4>
-				<div class="question">
-					<textarea name="experience" rows="5" cols="15" class="sm:w-full bg-gray-100 rounded mb-2 border border-gray-400 focus:outline-none focus:border-green-500 py-2 px-4"></textarea>
-				</div>
-			</fieldset>
-			<div class="submit">
-				<button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">Submit</button>
-			</div>
-		</form>
 		<h3 class="text-gray-900 font-medium font-title my-4 leading-tight">Join AGREA Events as a Volunteer</h3>
 		<div class="w-12 h-1 bg-green-500 rounded mt-2 mb-4"></div>
 		<p class="text-gray-900 my-4">Click the photo below to see details on how to join.</p>
