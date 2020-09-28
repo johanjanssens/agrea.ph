65c6b128b7c755abf59ebda9d930ed19a899d263
Johan Janssens <johan@janssens.me>
2020-09-28 23:27:18 +0200

    Preload the image to improve visual performance

--- sites/agrea.ph/partials/articles/single.html.php
+++ sites/agrea.ph/partials/articles/single.html.php
@@ -2,7 +2,7 @@
 <ktml:images max-width="80%">
 	<h1 itemprop="name" class="sm:text-5xl text-4xl font-medium font-title mb-2 text-gray-900 leading-none"><?= $article->title ?></h1>
 	<div class="h-1 w-20 bg-green-500 rounded mb-6"></div>
-	<img itemprop="image" class="rounded w-full mb-6" src="<?= $article->image->url ?>" alt="<?= $article->title; ?>">
+	<img itemprop="image" class="rounded w-full mb-6" src="<?= $article->image->url ?>" alt="<?= $article->title; ?>" data-preload="true">
 	<p class="mt-2 text-xs font-medium flex flex-row justify-between">
 		<span itemprop="author" itemscope itemtype="http://schema.org/Person">
 			<span itemprop="name"><?//= $article->getAuthor()->getName() ?></span>
