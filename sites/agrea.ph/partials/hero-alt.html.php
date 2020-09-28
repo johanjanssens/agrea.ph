1b51021c1d104e3cd81ab674f033d5e2ab89553e
waseemsadiq <waseem@bulletprooftemplates.com>
2020-09-28 15:27:46 +0100

    use only one main image for the hero - we lose the overlay but that’s preferable to the performance drain

--- sites/agrea.ph/partials/hero-alt.html.php
+++ sites/agrea.ph/partials/hero-alt.html.php
@@ -8,10 +8,8 @@
       'order' => 'desc',
       'limit' => 1,
     ]); ?>
-    <div class="relative z-10 md:flex md:justify-center md:w-3/5 md:mx-0 mx-8">
-        <div class="md:hidden mb-4">
-          <img class="object-cover object-center rounded" src="<?= $articles->image->url ?>" alt="<?= $articles->title; ?>">
-        </div>
+    <div class="flex flex-col-reverse lg:flex-row">
+      <div class="lg:w-3/5">
         <div class="hero-text">
           <h1 class="font-title sm:text-5xl text-3xl mb-0 font-medium text-dark-green-800 text-opacity-50 leading-none"><?= $articles->title ?></h1>
           <div class="mb-4 text-dark-green-800 text-opacity-50"><?= $articles->excerpt ?></div>
@@ -19,11 +17,10 @@
               <a href="<?= route(page('news/article'), ['slug' => $articles->slug]) ?>" class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Read More</a>
           </div>
         </div>
-    </div>
-  </div>
-  <div class="hero-image hidden md:block absolute top-0 right-0 z-10 h-full w-2/5">
-      <div style="background: url(<?= helper('image.url', $articles->image->url.'?h=400&fm=pjpeg') ?>) right center/cover;" class="h-full">
-          <div class="h-full bg-gray-900 opacity-25"></div>
       </div>
+      <div class="hero-image mb-16 lg:absolute lg:top-0 lg:right-0 lg:h-full lg:w-2/5">
+        <img class="rounded lg:h-full lg:object-cover lg:object-right" src="<?= $articles->image->url ?>" alt="<?= $articles->title; ?>">
+      </div>
+    </div>
   </div>
 </section>
