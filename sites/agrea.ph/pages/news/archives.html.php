1b822209051f5c2b63d4928614b3e1928d447c22
waseemsadiq <waseem@bulletprooftemplates.com>
2020-10-08 13:57:35 +0100

    Fix grid cell display

--- sites/agrea.ph/pages/news/archives.html.php
+++ sites/agrea.ph/pages/news/archives.html.php
@@ -33,22 +33,20 @@ collection:
 					? 'news'
 					: $article->category->slug;
 			?>
-			<aside role="gridcell">
-				<a class="bg-gray-100 hover:bg-lime-500 transition duration-300 ease-in-out text-gray-900 hover:text-dark-green-500 p-6 rounded-lg" href="<?= route('news/article', ['slug' => $article->slug]) ?>">
-					<img class="h-40 rounded w-full object-cover object-center mb-6" src="<?= $article->image->url ?>" alt="<?= $article->title; ?>">
-					<p class="leading-relaxed text-xs font-medium mb-2 flex items-center">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
-						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-						</svg> <?= date($article->published_date, 'd M, Y'); ?>
-					</p>
-					<p><a href="<?= route(page($category_slug.'/article'), ['slug' => $article->slug]) ?>"><?= $article->category->name; ?></a></p>
-					<h2 role="heading" aria-level="2" class="text-lg font-medium font-title mb-4 leading-tight"><?= $article->title; ?></h2>
-					<div class="mb-2"><?= $article->excerpt; ?></div>
-					<p class="text-xs mt-2">
-						Written by: <?= $article->getAuthor()->getName(); ?>
-					</p>
-				</a>
-			</aside>
+			<a role="gridcell" class="bg-gray-100 hover:bg-lime-500 transition duration-300 ease-in-out text-gray-900 hover:text-dark-green-500 p-6 rounded-lg" href="<?= route('news/article', ['slug' => $article->slug]) ?>">
+				<img class="h-40 rounded w-full object-cover object-center mb-6" src="<?= $article->image->url ?>" alt="<?= $article->title; ?>">
+				<p class="leading-relaxed text-xs font-medium mb-2 flex items-center">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
+					  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+					</svg> <?= date($article->published_date, 'd M, Y'); ?>
+				</p>
+				<p><a href="<?= route(page($category_slug.'/article'), ['slug' => $article->slug]) ?>"><?= $article->category->name; ?></a></p>
+				<h2 role="heading" aria-level="2" class="text-lg font-medium font-title mb-4 leading-tight"><?= $article->title; ?></h2>
+				<div class="mb-2"><?= $article->excerpt; ?></div>
+				<p class="text-xs mt-2">
+					Written by: <?= $article->getAuthor()->getName(); ?>
+				</p>
+			</a>
 			<? endforeach; ?>
 		</div>
 		<?= helper('paginator.pagination') ?>
