f04dd472cd6736dbb1a9b8396e514e018d92a486
waseemsadiq <waseem@bulletprooftemplates.com>
2020-10-12 17:32:02 +0100

    Add dates properly - use Pages’ date functionality

--- sites/agrea.ph/pages/index.html.php
+++ sites/agrea.ph/pages/index.html.php
@@ -232,7 +232,7 @@ title: Agrea - Ecology of Dignity
               <p class="leading-relaxed text-xs font-medium mb-4 flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 mr-1" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg> <span><?= $weblink->date ?></span>
+                </svg> <span><?= date($weblink->date, 'd M, Y'); ?></span>
               </p>
               <h3 role="heading" aria-level="3" class="text-lg font-medium font-title mb-4 leading-tight"><?= $weblink->title ?></h3>
             </aside>
