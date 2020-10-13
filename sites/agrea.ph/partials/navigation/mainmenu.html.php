ce3e33ca4049fcb8fedec00d0da7b3e330153bc2
waseemsadiq <waseem@bulletprooftemplates.com>
2020-10-13 17:11:19 +0100

    dark-mode: is not the correct syntax, it should be dark:

--- sites/agrea.ph/partials/navigation/mainmenu.html.php
+++ sites/agrea.ph/partials/navigation/mainmenu.html.php
@@ -1,4 +1,4 @@
 <? $pages = collection('pages', ['folder' => $folder ?? '.', 'level' => 3,  'recurse' => 'true', 'filter' => ['visible' => 'true']])  ?>
 <? foreach ($pages as $page) :?>
-<a class="px-4 py-2 mt-2 font-semibold bg-transparent rounded dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white md:mt-0 md:ml-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600<?= strpos(page()->path, $page->path) === 0 ? ' text-white bg-dark-green-500 is-active' : 'dark-mode:text-gray-200' ?>" href="<?= route($page); ?>"><?= $page->name; ?></a>
+<a class="px-4 py-2 mt-2 font-semibold bg-transparent rounded dark:bg-transparent dark:focus:text-white dark:hover:text-white md:mt-0 md:ml-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline dark:focus:bg-gray-600 dark:hover:bg-gray-600<?= strpos(page()->path, $page->path) === 0 ? ' text-white bg-dark-green-500 is-active' : 'dark:text-gray-200' ?>" href="<?= route($page); ?>"><?= $page->name; ?></a>
 <? endforeach ?>
