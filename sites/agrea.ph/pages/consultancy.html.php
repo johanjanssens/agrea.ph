948664db89ecd800957a78e859de89f254adb895
waseemsadiq <waseem@bulletprooftemplates.com>
2020-10-17 02:19:04 +0100

    Add a privacy policy page and a missing </ktml:images> tag

--- sites/agrea.ph/pages/consultancy.html.php
+++ sites/agrea.ph/pages/consultancy.html.php
@@ -180,11 +180,12 @@ slug: consultancy
 	</ktml:images>
 	</article>
 	<aside role="complimentary" class="sm:w-1/4 sm:mt-0 ms:pt-0 sm:pl-8 sm:pb-8 sm:border-l sm:mt-0 border-gray-300 sm:border-t-0 border-t mt-4 pt-4">
-		<ktml:images max-width="25%" lazyload="progressive,inline">
+	<ktml:images max-width="25%" lazyload="progressive,inline">
         <p><img class="rounded w-full" src="images://consultancy/atilano.jpg" /></p>
         <p>Founded by multi-awarded Filipina entrepreneur Cherrie Atilano, AGREA is a Filipino agriculture-focused, for-purpose, inclusive business that empowers farmers and their communities as impactful agents of change.</p>
         <h3 role="heading" aria-level="3" class="font-medium font-title mt-4 text-gray-900 dark:text-gray-100 text-lg leading-tight">Consult with AGREA</h3>
         <div class="w-12 h-1 bg-brand rounded mt-2 mb-4"></div>
         <p>To find more about our consultancy services please <a href="/about/contact">contact us</a> and send us a message.</p>
+    </ktml:images>
 	</aside>
 </div>
---
+++ sites/agrea.ph/pages/privacy.html.php
@@ -0,0 +1,31 @@
+---
+layout:
+    path: default
+    footer:
+        image: images://background/farmer-couple.jpg
+name: Privacy
+title: Privacy Statement
+summary: TODO
+visible: false
+---
+
+<div itemscope itemtype="http://schema.org/Article" class="flex flex-col sm:flex-row">
+	<article role="main" class="sm:w-3/4 sm:pr-8 sm:pt-4 sm:pb-4">
+	<ktml:images max-width="80%" lazyload="progressive,inline">
+		<div class="flex flex-wrap w-full mb-10">
+			<div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+				<h1 role="heading" aria-level="1" itemprop="name" class="sm:text-5xl text-4xl font-medium font-title mb-2 text-gray-900 dark:text-gray-100 leading-none"><?= $title ?></h1>
+				<div class="h-1 w-20 bg-brand rounded"></div>
+			</div>
+			<div class="lg:w-1/2 w-full">
+				<img class="md:h-36 rounded w-full object-cover object-center" src="images://consultancy/consultancy-1.jpg" alt="<?= $title ?>" data-max-width="50%" data-lazyload="progressive,inline">
+			</div>
+		</div>
+		<p>[TODO]: Add a privacy statement</p>
+	</ktml:images>
+	</article>
+	<aside role="complimentary" class="sm:w-1/4 sm:mt-0 ms:pt-0 sm:pl-8 sm:pb-8 sm:border-l sm:mt-0 border-gray-300 sm:border-t-0 border-t mt-4 pt-4">
+	<ktml:images max-width="25%" lazyload="progressive,inline">
+	</ktml:images>
+	</aside>
+</div>
