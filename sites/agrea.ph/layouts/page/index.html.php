d4fa1bd776acabf2b09cbbf9a743624d159ee063
waseemsadiq <waseem@bulletprooftemplates.com>
2020-10-26 21:32:17 +0000

    Remove fade in on page load and remove css for broken images

--- sites/agrea.ph/layouts/page/index.html.php
+++ sites/agrea.ph/layouts/page/index.html.php
@@ -3,7 +3,6 @@ layout: index
 footer:
     image: images://background/rice-farmer.jpg
 ---
-<svg id="fader"></svg>
 <main id="top" class="flex flex-col text-base bg-white dark:bg-black body-font text-gray-700 dark:text-gray-100 leading-relaxed shadow-lg">
 
     <?= import('/partials/structure/header'); ?>
