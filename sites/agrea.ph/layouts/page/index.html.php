dfb73187c70b469dccd7b8fa0fadbf4d576de634
waseemsadiq <waseem@bulletprooftemplates.com>
2020-10-29 16:47:46 +0000

    rename partials/structure/ to partials/page for consistency

--- sites/agrea.ph/layouts/page/index.html.php
+++ sites/agrea.ph/layouts/page/index.html.php
@@ -5,10 +5,10 @@ footer:
 ---
 <div id="top" class="main-wrapper flex flex-col text-base bg-white dark:bg-black body-font text-gray-700 dark:text-gray-100 leading-relaxed shadow-lg">
 
-    <?= import('/partials/structure/header'); ?>
+    <?= import('/partials/page/header'); ?>
 
     <ktml:content>
 
 </div>
 <!-- Footer content -->
-<?= import('/partials/structure/footer'); ?>
+<?= import('/partials/page/footer'); ?>
