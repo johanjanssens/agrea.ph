8a9314bbf29a71ff5f71153b56fffcbc6d5a4380
waseemsadiq <waseem@bulletprooftemplates.com>
2020-11-09 20:47:14 +0000

    add a class to the link to it doesn’t get underlined on hover

--- sites/agrea.ph/partials/hero-alt.html.php
+++ sites/agrea.ph/partials/hero-alt.html.php
@@ -11,7 +11,7 @@
     <div class="flex flex-col-reverse lg:flex-row">
       <div class="lg:w-3/5">
         <div class="hero-text">
-          <h1 class="font-title uppercase sm:text-5xl text-3xl mb-0 font-medium text-dark-green-800 text-opacity-50 leading-none"><a class="" href="<?= route(page('news/article'), ['slug' => $articles->slug]) ?>" title="Read the full story: <?= $articles->title ?>"><?= $articles->title ?></a></h1>
+          <h1 class="font-title uppercase sm:text-5xl text-3xl mb-0 font-medium text-dark-green-800 text-opacity-50 leading-none"><a class="leading-none" href="<?= route(page('news/article'), ['slug' => $articles->slug]) ?>" title="Read the full story: <?= $articles->title ?>"><?= $articles->title ?></a></h1>
           <div class="my-4 text-lg text-dark-green-800 text-opacity-50"><?= $articles->excerpt ?></div>
         </div>
       </div>
