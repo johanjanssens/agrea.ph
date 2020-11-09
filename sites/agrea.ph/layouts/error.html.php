223e209b52398fcdd5aee747e07481d16bf62b15
waseemsadiq <waseem@bulletprooftemplates.com>
2020-11-09 16:48:57 +0000

    reduce the image quailty and size using MIX

--- sites/agrea.ph/layouts/error.html.php
+++ sites/agrea.ph/layouts/error.html.php
@@ -18,6 +18,7 @@ layout: index
     z-index: -1;
   }
   .notfound .notfound-404 h1 {
+    background-image: url(<?= helper('image.url', 'images://background/login-bg.jpg?auto=compress&q=10&w=650') ?>);
     font-size: 112px;
     position: absolute;
     left: 50%;
@@ -38,9 +39,9 @@ layout: index
   }
 </style>
 <div class="relative h-screen bg-gray-50 dark:bg-gray-900">
-	<div class="notfound max-w-6xl">
+	<div class="notfound max-w-3xl">
 		<div class="notfound-404 mb-12">
-			<h1 style="background-image: url('images://background/login-bg.jpg?q=25');" class="bg-no-repeat bg-cover bg-center font-title font-black text-gray-700 dark:text-gray-200">Oops!</h1>
+			<h1 class="bg-no-repeat bg-cover bg-center font-title font-black text-gray-700 dark:text-gray-200">Oops!</h1>
 		</div>
 		<h2>Sorry we couldn't find this page</h2>
 		<p class="my-6">The page you are looking for might have been removed, had its name changed or it might be temporarily unavailable.</p>
