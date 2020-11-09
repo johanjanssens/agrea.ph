91639aeff5d774193de84c03361d35cbc931d1ad
waseemsadiq <waseem@bulletprooftemplates.com>
2020-11-09 16:21:27 +0000

    Adjust margins for mobile

--- sites/agrea.ph/layouts/error.html.php
+++ sites/agrea.ph/layouts/error.html.php
@@ -3,11 +3,11 @@ layout: index
 ---
 <div class="relative h-screen bg-gray-50 dark:bg-gray-900">
 	<div class="notfound max-w-6xl">
-		<div class="notfound-404 mb-24">
+		<div class="notfound-404 mb-12">
 			<h1 style="background-image: url('images://background/login-bg.jpg?q=25');" class="bg-no-repeat bg-cover bg-center font-title font-black text-gray-700 dark:text-gray-200">Oops!</h1>
 		</div>
 		<h2>Sorry we couldn't find this page</h2>
-		<p>The page you are looking for might have been removed, had its name changed or it might be temporarily unavailable.</p>
+		<p class="my-6">The page you are looking for might have been removed, had its name changed or it might be temporarily unavailable.</p>
 		<p>Don't worry, you can find plenty of other things on <a href="/">our homepage</a>.</p>
 	</div>
 </div>
