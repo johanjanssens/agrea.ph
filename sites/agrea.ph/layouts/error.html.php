5684d0346ad2ad1a57c383fdaf716247623c89be
waseemsadiq <waseem@bulletprooftemplates.com>
2020-11-09 16:02:57 +0000

    tweak the margin a wee bit

--- sites/agrea.ph/layouts/error.html.php
+++ sites/agrea.ph/layouts/error.html.php
@@ -3,7 +3,7 @@ layout: index
 ---
 <div class="relative h-screen bg-gray-50 dark:bg-gray-900">
 	<div class="notfound max-w-6xl">
-		<div class="notfound-404 mb-12">
+		<div class="notfound-404 mb-24">
 			<h1 style="background-image: url('images://background/login-bg.jpg?q=25');" class="bg-no-repeat bg-cover bg-center font-title font-black text-gray-700 dark:text-gray-200">Oops!</h1>
 		</div>
 		<h2>Sorry we couldn't find this page</h2>
