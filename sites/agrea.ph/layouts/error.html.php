cafed0c7ed4229fd0bfda421ee935f7105752a57
waseemsadiq <waseem@bulletprooftemplates.com>
2020-11-09 15:57:32 +0000

    Update the 404 page to something a bit nicer

--- sites/agrea.ph/layouts/error.html.php
+++ sites/agrea.ph/layouts/error.html.php
@@ -1,21 +1,13 @@
 ---
 layout: index
 ---
-
-<div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-	<div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-		<div class="flex flex-col overflow-y-auto md:flex-row">
-			<div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
-				<div class="w-full">
-					<h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">404</h1>
-					<p class="text-2xl md:text-3xl font-light leading-normal">Sorry we couldn't find this page. </p>
-					<p class="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-					<a href="/" class="bg-brand border-0 focus:outline-none hover:bg-green-600 inline-flex px-6 py-2 rounded text-lg text-white">back to homepage</a>
-				</div>
-			</div>
-			<div class="h-32 md:h-auto md:w-1/2">
-				<img aria-hidden="true" class="object-contain" src="images://background/farmer-couple.jpg" alt="farmer couple" />
-			</div>
+<div class="relative h-screen bg-gray-50 dark:bg-gray-900">
+	<div class="notfound max-w-6xl">
+		<div class="notfound-404 mb-12">
+			<h1 style="background-image: url('images://background/login-bg.jpg?q=25');" class="bg-no-repeat bg-cover bg-center font-title font-black text-gray-700 dark:text-gray-200">Oops!</h1>
 		</div>
+		<h2>Sorry we couldn't find this page</h2>
+		<p>The page you are looking for might have been removed, had its name changed or it might be temporarily unavailable.</p>
+		<p>Don't worry, you can find plenty of other things on <a href="/">our homepage</a>.</p>
 	</div>
 </div>
