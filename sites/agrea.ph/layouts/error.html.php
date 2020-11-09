14c9daa87f553d85842d5696cff1e5eaba0e08cd
waseemsadiq <waseem@bulletprooftemplates.com>
2020-11-09 16:39:01 +0000

    Move the css to the error layout, we don’t use it anywhere else, no need to load it everywhere

--- sites/agrea.ph/layouts/error.html.php
+++ sites/agrea.ph/layouts/error.html.php
@@ -1,6 +1,42 @@
 ---
 layout: index
 ---
+<style>
+  .notfound {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+    width: 100%;
+    text-align: center;
+  }
+  .notfound .notfound-404 {
+    height: 142px;
+    position: relative;
+    z-index: -1;
+  }
+  .notfound .notfound-404 h1 {
+    font-size: 112px;
+    position: absolute;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+        -ms-transform: translateX(-50%);
+            transform: translateX(-50%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-text-stroke: 2px var(--green-500);
+  }
+  @media only screen and (min-width: 768px) {
+    .notfound .notfound-404 {
+      height: 280px;
+    }
+    .notfound .notfound-404 h1 {
+      font-size: 230px;
+    }
+  }
+</style>
 <div class="relative h-screen bg-gray-50 dark:bg-gray-900">
 	<div class="notfound max-w-6xl">
 		<div class="notfound-404 mb-12">
